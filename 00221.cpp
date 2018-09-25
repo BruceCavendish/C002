b2e53a821da6d0246f8a56594b756aa044d2562d
Bruce Cavendish <43259903+BruceCavendish@users.noreply.github.com>
2018-09-25 11:38:03 +0800

    Add files via upload

---
+++ 00221.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int i;
+	double m,p=0;
+	double a[]={0,100000,200000,400000,600000,1000000};
+	double b[]={0.1,0.075,0.05,0.03,0.015,0.01};
+	printf("Àû  Èó£º\n  ");
+	scanf("%lf",&m);
+	for(i=0;i<6;i++)
+	{
+		if(m>a[i]&&i<5)
+		{
+			p+=(a[i+1]-a[i])*b[i];
+		}
+		else
+		{
+			p+=(m-a[i])*b[i];
+			break;
+		}
+	}
+	printf("½±½ðÎª£º\n  %.2f\n",p);
+	system("pause");
+	return 0;
+}
